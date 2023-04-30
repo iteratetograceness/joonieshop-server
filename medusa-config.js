a54659d3ba31a185f1f26189738c9d166d337e45
Jueun Grace Yun <graceyunn@gmail.com>
2023-04-29 23:34:00 -0400

    Clean up

--- medusa-config.js
+++ medusa-config.js
@@ -1,43 +1,41 @@
 const dotenv = require('dotenv')
 
-let ENV_FILE_NAME = '';
+let ENV_FILE_NAME = ''
 switch (process.env.NODE_ENV) {
-	case 'production':
-		ENV_FILE_NAME = '.env.production';
-		break;
-	case 'staging':
-		ENV_FILE_NAME = '.env.staging';
-		break;
-	case 'test':
-		ENV_FILE_NAME = '.env.test';
-		break;
-	case 'development':
-	default:
-		ENV_FILE_NAME = '.env';
-		break;
+  case 'production':
+    ENV_FILE_NAME = '.env.production'
+    break
+  case 'staging':
+    ENV_FILE_NAME = '.env.staging'
+    break
+  case 'test':
+    ENV_FILE_NAME = '.env.test'
+    break
+  case 'development':
+  default:
+    ENV_FILE_NAME = '.env'
+    break
 }
 
 try {
-	dotenv.config({ path: process.cwd() + '/' + ENV_FILE_NAME });
-} catch (e) {
-}
+  dotenv.config({ path: process.cwd() + '/' + ENV_FILE_NAME })
+} catch (e) {}
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000";
+const ADMIN_CORS = process.env.ADMIN_CORS || 'http://localhost:7000'
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://192.168.1.153:8000";
+const STORE_CORS = process.env.STORE_CORS || 'http://192.168.1.153:8000'
 
 // Database URL (here we use a local database called medusa-development)
-const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost:5432";
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432'
 
 // Medusa uses Redis, so this needs configuration as well
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
 
 // Stripe keys
-const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "";
-const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "";
+const STRIPE_API_KEY = process.env.STRIPE_API_KEY || ''
+const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || ''
 
 const plugins = [
   `medusa-fulfillment-manual`,
